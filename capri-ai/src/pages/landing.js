e1fc3a9b8ea2b3be7b2a97f509436e3550b1b681
shibajyoti20 <8dashibajyoti@gmail.com>
2021-07-10 20:20:29 +0530

    landing page created

--- capri-ai/src/pages/landing.js
+++ capri-ai/src/pages/landing.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import Button from '../components/button/Button';
+import './landing.css';
 
 function Landing()
 {
     return(
-        <React.Fragment>
-            <Button>mybutton</Button>
-        </React.Fragment>
+            <div className="btn-wrapper">
+                <div className="btn-inside">
+                    <Link to="/manage">
+                        <Button buttonText={"Manage User"} classname={"user-btn"} type={"button"}/>
+                    </Link>
+                    <Link to="https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=h[…]rsations/message.readonly%20conversations/message.write">
+                        <Button buttonText={"Add User"} classname={"user-btn"} type={"button"}/>
+                    </Link>
+                </div>
+            </div>
     )
 }
 
