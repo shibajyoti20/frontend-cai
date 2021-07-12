cbd1d6ffa5860f42c123a9923cb96f8626b6a945
hrk2023 <hrkdasgupta025@gmail.com>
2021-07-13 03:40:46 +0530

    Modified Manage User Layout, Added Optional Filter Functionality for Searching, Added Validations for Search Parameters

--- capri-ai/src/pages/ManageUser/ManageUser.js
+++ capri-ai/src/pages/ManageUser/ManageUser.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import axios from 'axios';
+import DatePicker from 'react-datepicker';
 
+import 'react-datepicker/dist/react-datepicker.css';
 import './ManageUser.css';
-import data from '../../components/SampleData';
 
 import CustomButton from '../../components/button/Button';
 import LinkedAccountItem from '../../components/LinkedAccountItem/LinkedAccountItem';
@@ -11,24 +12,67 @@ function ManageUser(){
     const[agentid, setAgentid] = React.useState('');
     const[LinkedAccounts, setLinkedAccounts] = React.useState([]);
 
+    // States For Filters
+    const[StartDate, setStartDate] = React.useState('');
+    const[EndDate, setEndDate] = React.useState('');
 
-    let SubmitHandler = async(e) => {
+
+    let DateFormatter = (unformattedDate) => {
+        var d = new Date(unformattedDate),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2) 
+            month = '0' + month;
+        if (day.length < 2) 
+            day = '0' + day;
+
+        let date = [year, month, day].join('-');
+        return date;
+    }
+
+    const SubmitHandler = async(e) => {
         e.preventDefault();
-        let listarea = document.getElementById("account-list-area");
+
+        let listElement = document.getElementById('account-list-area');
+        let BaseUrl = `https://systems.capriai.us/listAllAccounts?agentID=${agentid}`;
 
 
-        let requestUrl = `https://systems.capriai.us/listAllAccounts?agentID=${agentid}`
-        try{
-            listarea.innerText = "Loading..."
-            const response  = await axios.get(requestUrl);
-            console.log(response.data);
+        if(DateFormatter(EndDate) === '1970-01-01') setEndDate('');
+        if(DateFormatter(StartDate) === '1970-01-01') setStartDate('');
+
+        // Setting Start Date and End Date
+        if(StartDate === '' && EndDate !== ''){
+            let endDate = DateFormatter(EndDate);
+            BaseUrl = `${BaseUrl}&sessionsStartDate=${endDate}&sessionsEndDate=${endDate}`;
+
+        }else if(StartDate !== '' && EndDate === ''){
+            let startDate = DateFormatter(StartDate);
+            let endDate = DateFormatter(new Date());
+
+            BaseUrl = `${BaseUrl}&sessionsStartDate=${startDate}&sessionsEndDate=${endDate}`;
+
+        }else if(StartDate !== '' && EndDate !== ''){
+            let startDate = DateFormatter(StartDate);
+            let endDate = DateFormatter(EndDate);
+
+            BaseUrl = `${BaseUrl}&sessionsStartDate=${startDate}&sessionsEndDate=${endDate}`;
         }
-        catch (err){
-            console.error(err);
-            setLinkedAccounts(data);
-            if(LinkedAccounts.length === 0){
-                listarea.innerText = "No Matching Results"
+        
+        try{
+            if(listElement !== null){
+                listElement.innerText = "Loading...";
             }
+
+            const response = await axios.get(BaseUrl);
+            if(response.status === 200){
+                setLinkedAccounts(response.data);
+            }else{
+                throw new Error("404");
+            }
+        }catch(err){
+            listElement.innerText = "No Result";
         }
     }
 
@@ -36,9 +80,10 @@ function ManageUser(){
         <div className="manage-user-wrapper">
             <div className="manage-user-left-wrapper">
                 <div className="form-wrapper">
-                    <h2 id="manageUser-form-header">Search with AgentID</h2>
+                    <h2 id="agentid-form-header">Search By Agent ID</h2>
                     <form className="agentid-form" onSubmit={(e) => SubmitHandler(e)}>
-                        <div className="form-group">
+                        <span className="glyphicon glyphicon-search agentid-glyph"></span>
+                        <div className="form-group agentid-form-group">
                             <input type="text" placeholder="Agent-ID"
                             className="form-control agentid-formfield" 
                             value={agentid} 
@@ -50,18 +95,71 @@ function ManageUser(){
                             buttonText={"Search"}/>
                         </div>
                     </form>
+
+                    {/* Date Filters */}
+                    {/* <div className="filter-header-wrapper">
+                        <h4><b>Filters</b></h4>
+                    </div> */}
+
+                    <div className="search-filters-form">
+                        <div className="form-group">
+                            <DatePicker className="search-filter"
+                            selected={StartDate} 
+                            onChange={(date) => setStartDate(date)}
+                            dateFormat = "yyyy-MM-dd"
+                            maxDate={new Date()}
+                            placeholderText="Start Date" />
+                        </div>
+                        {
+                            StartDate !== '' &&
+                            <div className="form-group">
+                                <button onClick={() => setStartDate('')}
+                                className="clear-btn">
+                                    X
+                                </button>
+                            </div>
+                        }
+                        <div className="form-group">
+                            <h4 id="divider-text">to</h4>
+                        </div>
+                        <div className="form-group">
+                            <DatePicker className="search-filter"
+                            selected={EndDate} 
+                            onChange={(date) => setEndDate(date)}
+                            dateFormat = "yyyy-MM-dd"
+                            maxDate={new Date()}
+                            placeholderText="End Date" />
+                        </div>
+                        {
+                            EndDate !== '' &&
+                            <div className="form-group">
+                                <button 
+                                onClick={() => setEndDate('')}
+                                className="clear-btn">
+                                    X
+                                </button>
+                            </div>
+                        }
+                    </div>
                 </div>
             </div>
             <div className="manage-user-right-wrapper">
                 <div className="connected-account-list-wrapper">
+                    {LinkedAccounts.length !== 0 &&
+                        <div className="result-number-wrapper">
+                            <span className="result-section-sideheader">
+                                {LinkedAccounts.length} matching results
+                            </span>
+                        </div>
+                    }   
                     {LinkedAccounts.length === 0 ? 
                     
                     <div className="status-text">
                         <h4 id="account-list-area">No Results</h4>
                     </div>
                     :
-                    LinkedAccounts.map((item) => (
-                        <LinkedAccountItem accountData={item}/>
+                    LinkedAccounts.map((item, index) => (
+                        <LinkedAccountItem accountData={item} key={index}/>
                     ))
                     
                     }
