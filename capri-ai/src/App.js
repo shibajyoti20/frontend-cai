1dbcc63593326df0a876b8511234868d6264c198
hrk2023 <hrkdasgupta025@gmail.com>
2021-07-13 19:13:20 +0530

    Added Navbar.js to App.js

--- capri-ai/src/App.js
+++ capri-ai/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import Landing from './pages/landing';
 import ManageUser from './pages/ManageUser/ManageUser';
+import Navbar  from './components/Navbar/Navbar';
 
 function App() {
   return (
     <Router>
 		<div className="App">
+			<Navbar />
 			<Switch>
 				<Route path="/" exact component={Landing} />
 				<Route path="/manage" component={ManageUser} />
---
+++ capri-ai/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from '../../static/assets/2.png';
+
+function Navbar(){
+    return(
+        <div className="navbar-wrapper">
+            <div className="navbar-brand-wrapper">
+                <img src={Logo} alt="capri-ai" style={{"width" : "150px","objectFit": "contain"}}/>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
